46d4731864883a9b91c5003fb605f586edb7e00f
M Salma <muhsalaa@gmail.com>
2024-01-08 09:40:29 +0700

    chore: add service

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import App from "./App.svelte";
+import { services } from "./service";
 
 const app = new App({
   target: document.getElementById("app")!,
 });
+services();
 
 export default app;
---
+++ src/service.ts
@@ -0,0 +1,7 @@
+// here to create service, like to embed event trigger, etc, to the host web.
+
+export const services = () => {
+  setInterval(() => {
+    console.log("mweheheheh");
+  }, 2000);
+};
